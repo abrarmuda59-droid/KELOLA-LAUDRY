2cbb336f3a98d39f5e977f4f0ad58eddbfad3a99
Arinal17 <arinalhaq170107@gmail.com>
2026-06-03 23:56:38 +0700

    Update main.c

--- main.c
+++ main.c
@@ -8,3 +8,24 @@ int main() {
     
     loadData(&antrian, &riwayat);
     
+int role = login();
+    if (role == 0) return 1;
+    
+    int pilih;
+    do {
+        if (role == 1) {
+            printf("\n====================================\n");
+            printf("          MENU ADMIN                \n");
+            printf("====================================\n");
+            printf("1. tambah laundry\n");
+            printf("2. proses laundry\n");
+            printf("3. edit pesanan\n");
+            printf("4. lihat antrian\n");
+            printf("5. lihat riwayat\n");
+            printf("6. daftar harga\n");
+            printf("7. tampilkan antrian berdasarkan jenis layanan\n");
+            printf("8. sorting berdasarkan berat\n");
+            printf("9. jadwal pengambilan\n");
+            printf("0. keluar\n");
+            printf("====================================\n");
+            printf("Pilih: ");
