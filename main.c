0f523b5e58e58c9a8f0f46f63864384f688e40e1
Syella25 <SYELLA@mhs.usk.ac.id>
2026-06-04 00:04:48 +0700

    Update main.c

--- main.c
+++ main.c
@@ -28,3 +28,33 @@ int main() {
             printf("0. keluar\n");
             printf("====================================\n");
             printf("Pilih: ");
+
+ if (scanf("%d", &pilih) != 1) {
+                printf("\nPilihan tidak valid!\n");
+                while (getchar() != '\n');
+                continue;
+            }
+            
+            switch(pilih) {
+                case 1: {
+                    char nama[50], jenis[10], tanggalMasuk[11], tanggalSelesai[11];
+                    int j, day, month, year;
+                    float berat;
+                    
+                    printf("\nNama: ");
+                    scanf(" %[^\n]", nama);
+                    printf("Berat (kg): ");
+                    scanf("%f", &berat);
+                    if (berat < 2) berat = 2;
+                    printf("Jenis (1.Reguler/2.Express): ");
+                    scanf("%d", &j);
+                    if (j != 1 && j != 2) {
+                        printf("\nPilihan tidak ada!\n");
+                        break;
+                    }
+                    strcpy(jenis, (j == 1) ? "reguler" : "express");
+                    
+                    int tanggalValid = 0;
+                    while (!tanggalValid) {
+                        printf("Tanggal Masuk (DD/MM/YYYY): ");
+                        scanf("%s", tanggalMasuk);
